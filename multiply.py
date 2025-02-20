d589b289d1fc613d54c083f95a13fea33eda6129
tcondie2 <Taloncondie@gmail.com>
2025-02-20 10:28:50 -0700

    Create multiply.py
    Takes two numbers and multiplies those numbers

---
+++ multiply.py
@@ -0,0 +1,8 @@
+# Talon Condie 
+# 2/20/2025
+# Take input and multiply it 
+
+# Takes two numbers and returns the product 
+def multiplyTwoNumbers(iNum1, iNum2) :
+    result=iNum1*iNum2
+    return result
